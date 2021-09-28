a5e4445a44d22c056dfcf95308a006231b622841
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-28 02:52:24 +0000

    no more checker bugs please, but uses printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
