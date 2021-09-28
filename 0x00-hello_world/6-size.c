9bcc480de8b19eeee30cf7ec0e92bd23c65a5d78
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-28 03:11:14 +0000

    added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+int intType;
+long longintType;
+long long longlongType;
+float floatType;
+char charType;
+
+printf("Size of a  char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
+}
