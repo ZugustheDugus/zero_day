1a04f1dbba51027be44d694e44bbb3448a7abd5e
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-28 02:20:25 +0000

    added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* This file will use the put function to print a phrase */
+int main()
+{
+  char string[];
+  strcpy(str, "Programming is like building a multilingual puzzle\n");
+  puts(string);
+  return(0);
+}
