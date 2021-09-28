e2b35b45eeec24b743b3446b92eca532fe1afeb6
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-28 22:30:13 +0000

    kek

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ printf("Last digit of %d is 8 and is greater than 5\n", n);
 }
 else if (n < 6 && n != 0)
 {
-printf("Last digit of %d is -8 and is less than 6 and is not 0\n", n);
+printf("Last digit of %d is -8 and is less than 6 and not 0\n", n);
 }
 else
 {
