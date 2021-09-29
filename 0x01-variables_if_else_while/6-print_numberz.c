40ba2f4a376055297fa44dcb42d91247b465216b
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-29 00:49:43 +0000

    asdf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+int i;
+for (i = '0' ; i <= '9' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
