42923a54b39fe16d865b181a6a98b86e153d5655
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-29 00:53:57 +0000

    asdf

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+int ch;
+for (ch = 'z' ; ch >= 'a' ; --ch)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
