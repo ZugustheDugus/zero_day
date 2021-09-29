bdab405cb77b3ed41d99f1d8f397b4070dbccdec
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-29 21:28:32 +0000

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - check the code.
+ * @n: the n thing that does the stuff
+ * Return: of the king
+ */
+int _abs(int n)
+{
+if (n > 0 || n < 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
