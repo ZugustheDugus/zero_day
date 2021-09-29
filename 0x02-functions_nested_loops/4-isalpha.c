e460b7787ba201b12a3c296ad9dd9ad180e519d3
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-29 20:59:49 +0000

    added 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - The function being run in the 3-main.c file
+ * @c: Description of parameter c
+ * Return: Returns 1 if upper case, 0 if lower case
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
