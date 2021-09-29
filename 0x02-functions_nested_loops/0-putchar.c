af8823bcb71f99d9505616f65067486d9f27aa16
ZugustheDugus <h1cooper2019@protonmail.com>
2021-09-29 19:21:33 +0000

    fixed syntax of comment and added main (void) function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
