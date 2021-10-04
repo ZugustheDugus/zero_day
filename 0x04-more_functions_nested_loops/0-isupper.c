d5de9ccc5abd91f1972dc6ccffa6e3069a3e059c
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-04 19:46:48 +0000

    ads

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - function to check if the letter is uppercase.
+ * @c: variable being checked for case
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (c >= 'a' && c <= 'c')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
