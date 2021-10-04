0731f383e7f4675d9ac8064b3fbd3089ea529454
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-04 20:07:44 +0000

    we did this already?

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - void function to print numbers using _putchar function
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = '0' ; c <= '9' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
