4d81ec6b27e5a33fc90dd17a4eb6c8b3b9813f5e
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-06 15:13:11 +0000

    prints in reverse, wrong putchar?

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: variable pointed to
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+while (*s != '\0')
+{
+putchar(*s);
+s++;
+}
+s--;
+while (*s != '\0')
+{
+putchar(*s);
+s--;
+}
+putchar('\n');
+}
