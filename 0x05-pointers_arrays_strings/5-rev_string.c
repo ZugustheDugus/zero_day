5b5f2f72be800e1fdd9f2f1e8a44a438563851bc
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-06 20:05:50 +0000

    holy operators

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,19 +3,19 @@
 /**
  * rev_string - check the code
  *
- * Return: Always 0.
+ * @s: character referring to str in main function
  */
 void rev_string(char *s)
 {
 char t;
 int i = 0, j = 0;
 
-while(*(s + i))
+while (*(s + i))
 {
 i++;
 }
 i--;
-while(i > j)
+while (i > j)
 {
 t = *(s + i);
 *(s + i) = *(s + j);
