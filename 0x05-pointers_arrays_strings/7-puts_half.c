0d70682b4b251590f5f79fa1e688be0402531f0c
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-07 00:33:52 +0000

    added task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ *
+ * @str: string
+ */
+void puts_half(char *str)
+{
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+i = i / 2;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}
