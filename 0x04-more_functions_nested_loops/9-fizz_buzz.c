4850f0b8d570f66b460a33fa202714dbf31a011f
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-07 19:36:20 +0000

    asdf

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,12 +7,17 @@
  * Return: Returns value of i if conditions not met
  */
 
-void main(void)
+int main(void)
 {
 int i = 1;
 while (i <= 100)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+i++;
+}
+else if (i % 3 == 0)
 {
 printf("Fizz");
 i++;
@@ -22,15 +27,16 @@ else if (i % 5 == 0)
 printf("Buzz");
 i++;
 }
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz");
-i++;
-}
 else
 {
 printf("%d", i);
 i++;
 }
+if (i != 100)
+{
+printf(" ");
 }
+}
+printf("\n");
+return (0);
 }
