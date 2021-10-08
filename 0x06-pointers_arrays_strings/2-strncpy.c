374cb6b538ab630bfeb97c94a866d50dea1c7012
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-08 23:16:21 +0000

    Read the manual and fixed memory reset stuff

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,20 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 i = 0;
-while (n > 0)
+while (src[i] && n > 0)
 {
 dest[i] = src[i];
 i++;
 n--;
+}
+if (n > 0)
+{
+while (n > 0)
+{
+dest[i] = '\0';
+i++;
+n--;
+}
 }
 return (dest);
 }
