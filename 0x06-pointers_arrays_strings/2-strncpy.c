1559d128ba1a13aa47c971803b00b7ef5c3b33f5
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-08 22:28:27 +0000

    added task 2 by modifying task 9 from 0x05 project

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: Original string to be modified into output
+ * @src: String to be appended to dest, in this case a copy
+ * @n: variable which indexes the number of bytes of dest to print
+ * Return: Returns finished, updated dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while (src[i] != '\0' && n--)
+{
+dest[i] = src[i];
+i++;
+}
+dest[i++] = '\0';
+return (dest);
+}
