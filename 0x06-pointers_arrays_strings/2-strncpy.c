beecbb9c5e7ecd609c70befb53cc546b24fa1186
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-08 22:48:05 +0000

    Readding task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 i = 0;
-while (src[i] != '\0' && n--)
+while (src[i] && n--)
 {
 dest[i] = src[i];
 i++;
 }
-dest[i++] = '\0';
+dest[i] = '\0';
 return (dest);
 }
