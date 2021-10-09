b7cb57d67f701b6f0ac9a33515455b7f45a861af
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-09 18:29:18 +0000

    Changed to unsigned char format

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,22 +10,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i, j;
-i = 0;
-j = 0;
-
-while (s1[i])
+while (*s1)
 {
-if (s1[i] != s2[j])
+if (*s1 != *s2)
 {
 break;
 }
-s1[i]++;
-s2[j]++;
-if (s1[i] == '\0' || s2[j] == '\0')
-{
-break;
+s1++;
+s2++;
 }
-}
-return (s1[i] - s2[j]);
+return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
