32460776307f8801c82360614b9afa731390ab99
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-11 00:53:36 +0000

    Adding tasks, just to have some checks

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+char *cap_string(char *)
+{
+  int i = 0, j;
+  while (s[i])
+    {
+      for (i < j,       
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+char *leet(char *)
+{
+  int i = 0, j = 0, t;
+  char n[15] = [a, A, e, E, o, O, t, T, l, T, 4, 3, 0, 7, 1];
+
+  while (s[i])
+    {
+      i++;
+    }
+  i--;
+  for (j = 0 ; j < i ; j++, i--)
+    {
+      s[i] = s[i] - 13
+      if (s[i] == '\0' || s[j] == '\0')
+	{
+	  break;
+	}
+    }
+}
