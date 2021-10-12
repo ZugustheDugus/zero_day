2b019a172a4516723061ac75e19f06c390190621
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-12 00:08:01 +0000

    added proper task 6 that does something

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,30 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * cap_string - Capitalizes the first letter of each word w/ word separators
+ * @s: Assignment of the char to the string / the string being pointed to
+ * Return: Returns the String
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
-  int i = 0, j;
-  while (s[i])
-    {
-      for (i < j,       
+int i, j;
+char sep[] = " \t\n,;.!?\"(){}"; /* word seperators */
+i = 1;
+if (s[0] >= 'a' && s[0] <= 'z')
+{
+s[0] = s[0] - 32;
+}
+while (s[i])
+{
+for (j = 0 ; sep[j] != '\0' ; j++)
+{
+if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+{
+s[i] = s[i] - 32;
+}
+}
+i++;
+}
+return (s);
+}
