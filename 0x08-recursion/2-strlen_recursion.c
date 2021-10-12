642f1bed00a972b16f8cb45b4e53874d82e11879
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-12 23:06:19 +0000

    added task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Outputs the length of string using recursion
+ * @s: The String whose length is being pointed to and returned
+ * Return: Either returns 0 or the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+else
+{
+return (*s);
+_strlen_recursion(s + 1);
+}
+}
