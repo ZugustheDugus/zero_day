1ca0de0be9afc2a585a29503d2aa5eb3df705c06
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-19 16:29:43 +0000

    added task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function which passes arguments
+ * @argc: argument to which is number of arguments
+ * @argv: argument which is char argument
+ * Return: Always returns success
+ */
+
+int main(int argc, char *argv[])
+{
+printf("argc = %d\n", argc);
+return (0);
+}
