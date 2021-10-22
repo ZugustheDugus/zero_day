08a0456af1d3addf087d21a0a549a1587221c6ef
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-22 03:41:40 +0000

    added a function to it

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,16 @@
 #ifndef ABS(x)
 #define ABS(x) ((x < 0) ? (-x) : (x))
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+cout <<ABS(x)<<end1;
+return (0);
+}
+
 #endif
