d112bdb818d69c2865d77501c250eda23cec1dfb
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-22 15:04:16 +0000

    added task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - return the pointer to the start of a string
+ * @str: The string being pointed to and duplicated
+ * Return: Return the string pointed to
+ */
+
+char *_strdup(char *str)
+{
+unsigned int n, l = 0;
+char *s;
+
+if (str == 0)
+{
+return (NULL);
+}
+
+while (str[l])
+{
+l++;
+}
+l++;
+
+s = malloc(l *sizeof(char));
+
+for (n = 0 ; n < l ; n++)
+{
+s[n] = str[n];
+}
+return (s);
+}
