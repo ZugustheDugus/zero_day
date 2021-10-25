52c122fa18fe362bee73d04613f0d2e3d6487daf
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-25 20:48:22 +0000

    added task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - checks if a malloc function worked
+ * @b: the address of memory being written
+ *
+ * Return: Returns NULL if 0 length integer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int *s;
+  
+s = malloc(b);
+
+if (s == NULL)
+{
+exit(98);
+}
+return (s);
+}
