0294694d8de471f6e5b1c8fb19a5978d853e81b8
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-25 21:02:20 +0000

    added task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings for n bytes
+ * @n: number of bytes to concatenate
+ * @s1: Primary string being added to
+ * @s2: Second string being added to Primary for n bytes
+ * Return: Returns the string after concatenation
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+char *s;
+unsigned int i, j, first = strlen(s1);
+
+if (s1 == 0 || s1 == NULL)
+{
+s1 = '\0';
+}
+if (s2 == 0 || s2 == NULL)
+{
+s2 = '\0';
+}
+
+s = malloc(sizeof(*s) * (first + n + 1));
+
+for (i = 0, j = 0 ; i < (first + n) ; i++)
+{
+if (i < first)
+{
+s[i] = s1[i];
+}
+else
+{
+s[i] = s2[j++];
+}
+}
+s[i] = '\0';
+return (s);
+}
