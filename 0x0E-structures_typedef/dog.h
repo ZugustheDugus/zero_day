be01461af5a8afee47b62a739b73d4b8a863c758
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-26 19:14:46 +0000

    betty'd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Creates a struct who is a very gud boi
+ * @name: variable for the dog name
+ * @age: variable for the age of the gud boi
+ * @owner: variable for who owns the gud boi
+ */
+
 struct dog
 {
 char *name;
