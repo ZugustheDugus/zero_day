b93e449772a991f7f0f00f7a9def7920a1c94bc7
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-26 20:53:35 +0000

    added task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - creates a new dog from struct dog
+ * @name: variable copying dog name
+ * @age: variable copying dog age (float)
+ * @owner: variable copying dog owner
+ * Return: newdog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+dog_t *newdog;
+
+newdog = malloc(sizeof(dog_t));
+
+if (newdog == NULL)
+{
+return (NULL);
+}
+newdog->name = name;
+newdog->age = age;
+newdog->owner = owner;
+return (newdog);
+
+}
