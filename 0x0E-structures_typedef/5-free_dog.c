6c89f289df1626cfd0acd501ebb7ab75a364fab2
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-26 21:03:06 +0000

    added task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return (NULL);
+
+free(dog_t->d);
+}
