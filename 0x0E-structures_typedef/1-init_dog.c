b44839ab9f15f0b8fa5d08d036d2a17545c83eb7
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-26 19:45:33 +0000

    added task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * init_dog - Populates struct dog with information
+ * struct dog - the data structure containing slots for variables to fill
+ * @d: variable to hold the place of struct dog
+ * @name: name variable to populate spot in struct
+ * @age: age variable to populate spot in struct
+ * @owner: owner variable to populate spot in struct
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+if (!d)
+{
+return;
+}
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
