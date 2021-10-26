15fcae645643261fd55e6bdb7e2b94495dd70493
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-26 20:55:20 +0000

    added task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,9 +18,17 @@ dog_t *newdog;
 newdog = malloc(sizeof(dog_t));
 
 if (newdog == NULL)
-{
 return (NULL);
-}
+
+if (name == NULL)
+return (NULL);
+
+if (age == '\0')
+return (NULL);
+
+if (owner == NULL)
+return (NULL);
+ 
 newdog->name = name;
 newdog->age = age;
 newdog->owner = owner;
