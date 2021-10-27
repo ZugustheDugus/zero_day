e63b6317a13298f270ac20a6771d10aee5c26e8c
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-27 01:26:17 +0000

    added _strdup function to task to copy name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * new_dog - creates a new dog from struct dog
@@ -18,20 +19,69 @@ dog_t *newdog;
 newdog = malloc(sizeof(dog_t));
 
 if (newdog == NULL)
+{
+free(newdog);
 return (NULL);
+}
 
 if (name == NULL)
+{
+free(newdog);
 return (NULL);
+}
 
 if (age == '\0')
+{
+free(newdog);
 return (NULL);
+}
 
 if (owner == NULL)
+{
+free(newdog);
 return (NULL);
+}
 
-newdog->name = name;
+newdog->name = _strdup(name);
+newdog->owner = _strdup(owner);
+
 newdog->age = age;
-newdog->owner = owner;
+
 return (newdog);
 
+}
+
+/**
+ * _strdup - return the pointer to the start of a string
+ * @str: The string being pointed to and duplicated
+ * Return: Return the string pointed to
+ */
+
+char *_strdup(char *str)
+{
+unsigned int n, l = 0;
+char *s;
+
+if (str == 0)
+{
+return (NULL);
+}
+
+while (str[l])
+{
+l++;
+}
+l++;
+
+s = malloc(l *sizeof(char));
+if (s == 0 || s == NULL)
+{
+return (NULL);
+}
+
+for (n = 0 ; n < l ; n++)
+{
+s[n] = str[n];
+}
+return (s);
 }
