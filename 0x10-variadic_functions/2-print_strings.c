5fcc1b54aa5a3a691dc5bb588fdf29c7addbe4a1
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-29 22:48:04 +0000

    asdf

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,22 +11,24 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *t;
 	va_list s;
 
-	if (separator == NULL)
-		return;
-
 	va_start(s, n);
-
 	for (i = 0; i < n; i++)
 	{
-		if (i == (n - 1))
+		t = va_arg(s, char *);
+		if (t != NULL)
 		{
-			printf("%s", va_arg(s, const char *));
+			printf("%s", t);
 		}
 		else
 		{
-		    printf("%s%s", va_arg(s, const char *), separator);
+			printf("(nil)");
+		}
+		if (i < (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
