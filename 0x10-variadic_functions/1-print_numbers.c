46af171d884fa5c5ab7b75c63c9b01826040abff
ZugustheDugus <h1cooper2019@protonmail.com>
2021-10-29 22:50:07 +0000

    changed if statement to check for null separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,15 +13,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list s;
 
-	if (separator == NULL)
-		return;
-
 	va_start(s, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(s, int));
-		if (i < (n - 1))
+		if (i < (n - 1) && separator != NULL)
 			{
 			printf("%s", separator);
 			}
